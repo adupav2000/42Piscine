fbf74daa7de771bcab74da83f0c870c2e1c58f10
alain <adupav2000@gmail.com>
2019-08-07 12:43:34 +0200

    first commit

--- day05/adu-pavi/ex00/ft_strcmp.c
+++ day05/adu-pavi/ex00/ft_strcmp.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/06 11:38:33 by adu-pavi          #+#    #+#             */
-/*   Updated: 2019/08/07 12:41:41 by adu-pavi         ###   ########.fr       */
+/*   Created: 2019/08/06 11:33:43 by adu-pavi          #+#    #+#             */
+/*   Updated: 2019/08/06 11:35:15 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
-	while (s2[i] && s1[i] && i <= n)
+	while (s2[i] && s1[i])
 	{
 		if ((s1[i] - s2[i]))
 		{
--- day05/adu-pavi/ex05/ft_strlcat.c
+++ day05/adu-pavi/ex05/ft_strlcat.c
@@ -1,27 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/06 11:38:33 by adu-pavi          #+#    #+#             */
-/*   Updated: 2019/08/07 12:41:41 by adu-pavi         ###   ########.fr       */
+/*   Created: 2019/08/06 17:26:59 by adu-pavi          #+#    #+#             */
+/*   Updated: 2019/08/06 20:26:35 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int				ft_strlen(char *str)
 {
-	unsigned int i;
+	int	length;
 
-	i = 0;
-	while (s2[i] && s1[i] && i <= n)
+	length = 0;
+	while (str[length] != 0)
 	{
-		if ((s1[i] - s2[i]))
-		{
-			return (s1[i] - s2[i]);
-		}
+		length++;
+	}
+	return (length);
+}
+
+unsigned	int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int i_max;
+	int i;
+	int ret;
+	int s;
+
+	i_max = size - ft_strlen(dest);
+	i = 0;
+	ret = ft_strlen(dest) + ft_strlen(src) - 1;
+	while (src[i] && i < i_max)
+	{
+		s = ft_strlen(dest) + 1;
+		dest[s] = src[i];
 		i++;
 	}
-	return (0);
+	return (ret);
 }
