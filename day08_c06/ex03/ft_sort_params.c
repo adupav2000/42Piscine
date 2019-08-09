824775c81892ba0956ba1c25ef108b0d3eea61bb
alain <adupav2000@gmail.com>
2019-08-09 16:56:39 +0200

    es

--- day08_c06/ex03/ft_sort_params.c
+++ day08_c06/ex03/ft_sort_params.c
@@ -1,14 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/09 16:30:27 by adu-pavi          #+#    #+#             */
-/*   Updated: 2019/08/09 16:53:07 by adu-pavi         ###   ########.fr       */
+/*   Created: 2019/08/09 16:54:06 by adu-pavi          #+#    #+#             */
+/*   Updated: 2019/08/09 16:56:22 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 
 #include <unistd.h>
@@ -32,14 +33,14 @@ void	ft_putstr(char *str)
 
 int get_case_in_ascii_order(int case_to_get, char *str)
 {
-	int i[3];
+	int i[4];
 
 	i[0] = 32;
 	i[1] = 0;
 	i[3] = 0;
 	while (i[0] < case_to_get)
 	{
-		while (&str[i[3]] != i[0])
+		while (str[i[3]] != i[0])
 		{
 			i[3]++;
 		}
@@ -52,12 +53,11 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	i = argc - 1;
-	while (argv[i] && i > 0)
+	i = 0;
+	while (argc[i])
 	{
-		ft_putstr(argv[i]);
-		ft_putchar('\n');
-		i--;
+		get_case_in_ascii_order(i, *argv);
+		i++;
 	}
 	return 0;
 }
