c69345c6b6ae5a15d9f0615f1e1d24337fc02f2d
alain <adupav2000@gmail.com>
2019-08-11 11:48:23 +0200

    sunday morning

--- rush01/try_forRush01.c
+++ rush01/try_forRush01.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+int	ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (str[length] != 0)
+	{
+		length++;
+	}
+	return (length);
+}
 
 int main(int argc, char const *argv[])
 {
-	int size_table = argc - 1;
+	int size_table = ft_strlen(argv[1]);
 	int table[size_table][size_table];
+	int i = 0;
+	
 	while ()
 	{
-		
+
 	}
 	return 0;
 }
