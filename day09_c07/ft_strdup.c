0e46a9bc8753329350d0045b7b607dac6720de3c
alain <adupav2000@gmail.com>
2019-08-11 11:44:48 +0200

    sunday morning

---
+++ day09_c07/ft_strdup.c
@@ -0,0 +1,11 @@
+
+
+
+#include <stdlib.h>
+char	*ft_strdup(char *src)
+{
+    char **st;
+
+    st = malloc(sizeof(st));    
+	return st
+}
---
+++ rush01/try_forRush01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int size_table = argc - 1;
+	int table[size_table][size_table];
+	while ()
+	{
+		
+	}
+	return 0;
+}
