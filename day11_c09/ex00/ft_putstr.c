20ae5c3ff6015c78b82f44f0622fb6d87a4cc224
alain <adupav2000@gmail.com>
2019-08-20 23:11:07 +0200

    changing ft_putstr

--- day11_c09/ex00/ft_putstr.c
+++ day11_c09/ex00/ft_putstr.c
@@ -6,16 +6,11 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 18:03:18 by adu-pavi          #+#    #+#             */
-/*   Updated: 2019/08/14 18:32:46 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2019/08/20 23:10:37 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_putstr(char *str)
 {
@@ -24,7 +19,7 @@ void	ft_putstr(char *str)
 	length = 0;
 	while (str[length] != 0)
 	{
-		ft_putchar(str[length]);
+		write(1, &str[length], 1);
 		length++;
 	}
 }
